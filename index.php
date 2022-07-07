7c7203e4754b297671ed350f2bf4d5e1a82b4635
nobert2 <53371848+nobert2@users.noreply.github.com>
2022-07-07 10:48:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,13 @@
   <title>UWEZO FUND | Log in</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
+  <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
+
+  <!-- Load our React component. -->
+  <script src="like_button.js"></script>
   </head>
   <body>
-    <p>Hello World</p>
+    <div id="like_button_container"></div>
   </body>
 </html>
